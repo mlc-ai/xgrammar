d61fde2740716a0ca8fbe64eda554302f18b9cc3
Charlie Ruan <53290280+CharlieFRuan@users.noreply.github.com>
2024-11-20 17:54:36 -0500

    [Contrib] Impelment HF transformers LogitsProcessor for integration

---
+++ examples/transformers_example.py
@@ -0,0 +1,46 @@
+import xgrammar as xgr
+import torch
+
+from xgrammar.contrib.transformers import LogitsProcessor
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
+
+# Instantiate model
+model_name = "/ssd1/cfruan/xgrammar/Qwen2.5-0.5B-Instruct"
+model = AutoModelForCausalLM.from_pretrained(
+    model_name, torch_dtype=torch.float32, device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+config = AutoConfig.from_pretrained(model_name)
+
+# Instantiate LogitProcessor
+tokenizer_info = xgr.TokenizerInfo.from_huggingface(tokenizer)
+full_vocab_size = config.vocab_size
+grammar_compiler = xgr.CachedGrammarCompiler(tokenizer_info, max_threads=1)
+compiled_grammar = grammar_compiler.compile_json_grammar()
+logits_processor = xgr.contrib.transformers.LogitsProcessor(
+    compiled_grammar, tokenizer_info, full_vocab_size
+)
+
+# Inference
+prompt = "Introduce yourself in JSON."
+messages = [
+    {
+        "role": "system",
+        "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant.",
+    },
+    {"role": "user", "content": prompt},
+]
+text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    **model_inputs, max_new_tokens=512, logits_processor=[logits_processor]
+)
+generated_ids = [
+    output_ids[len(input_ids) :]
+    for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print(response)
---
+++ python/xgrammar/contrib/transformers.py
@@ -0,0 +1,70 @@
+import transformers
+import xgrammar as xgr
+import torch
+
+
+class LogitsProcessor(transformers.LogitsProcessor):
+    """
+    LogitsProcessor for processing logits in transformers' generate() method.
+
+    Example usage:
+        ```python
+        model_id = "Qwen/Qwen2.5-1.5B-Instruct"
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        config = AutoConfig.from_pretrained(model_id)
+        tokenizer_info = xgr.TokenizerInfo.from_huggingface(tokenizer)
+        # This can be larger than tokenizer.vocab_size due to paddings
+        full_vocab_size = config.vocab_size
+
+        grammar_compiler = xgr.CachedGrammarCompiler(tokenizer_info, max_threads=1)
+        compiled_grammar = grammar_compiler.compile_json_grammar()
+        logits_processor = xgr.contrib.transformers.LogitsProcessor(
+          compiled_grammar, tokenizer_info, full_vocab_size
+        )
+        model.generate(prompt, logit_processor=logits_processor)
+        ```
+
+        For an end-to-end example, see `examples/transformers_example.py`.
+    """
+
+    def __init__(
+        self,
+        compiled_grammar: xgr.CompiledGrammar,
+        tokenizer_info: xgr.TokenizerInfo,
+        full_vocab_size: int,
+    ):
+        """Initialize the LogitsProcessor.
+
+        Parameters
+        ----------
+        compiled_grammar : xgr.CompiledGrammar
+            A grammar compiled according to the given grammar and the model's tokenizer_info.
+        tokenizer_info : xgr.TokenizerInfo
+            The tokenizer information of the model to be used.
+        full_vocab_size : int
+            The full vocab size of the model (AutoConfig.vocab_size).
+        """
+        self.matcher = xgr.GrammarMatcher(
+            compiled_grammar, tokenizer_info, vocab_size=full_vocab_size
+        )
+        self.token_bitmask = xgr.GrammarMatcher.allocate_token_bitmask(self.matcher.vocab_size)
+        self.prefilled = False
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
+        """
+        Accept token sampled in the last iteration, fill in bitmask, and apply bitmask to logits.
+        """
+        if scores.device.type != "cuda":
+            raise ValueError("logits must be on CUDA")
+
+        if not self.prefilled:
+            # Have not sampled a token yet
+            self.prefilled = True
+        else:
+            sampled_token = input_ids[0][-1]
+            assert self.matcher.accept_token(sampled_token)
+
+        self.matcher.fill_next_token_bitmask(self.token_bitmask)
+        self.matcher.apply_token_bitmask_inplace(scores, self.token_bitmask)
+
+        return scores
