9f998819d2758d91e169628cf37d4358f912264a
DarkSharpness <2040703891@qq.com>
2025-03-27 08:44:55 +0000

    feat: complete nanobind and serializer

---
+++ python/xgrammar/serializer.py
@@ -0,0 +1,53 @@
+"""Serializing and deserializing grammars and tokenizers."""
+
+from typing import List, Union
+
+from .base import XGRObject, _core
+from .compiler import CompiledGrammar
+from .grammar import Grammar
+from .tokenizer_info import TokenizerInfo
+
+
+class JSONSerializer:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def serialize_grammar(grammar: Grammar, prettify: bool = False) -> str:
+        return _core.JSONSerializer.serialize_grammar(grammar._handle, prettify)
+
+    @staticmethod
+    def deserialize_grammar(serialized_grammar: str) -> Grammar:
+        return Grammar._create_from_handle(
+            _core.JSONSerializer.deserialize_grammar(serialized_grammar)
+        )
+
+    @staticmethod
+    def serialize_tokenizer_info(tokenizer_info: TokenizerInfo, prettify: bool = False) -> str:
+        return _core.JSONSerializer.serialize_tokenizer_info(tokenizer_info._handle, prettify)
+
+    @staticmethod
+    def deserialize_tokenizer_info(
+        serialized_tokenizer_info: str, encoded_vocab: List[Union[bytes, str]]
+    ) -> TokenizerInfo:
+        return TokenizerInfo._create_from_handle(
+            _core.JSONSerializer.deserialize_tokenizer_info(
+                serialized_tokenizer_info, encoded_vocab
+            )
+        )
+
+    @staticmethod
+    def serialize_compiled_grammar(
+        compiled_grammar: CompiledGrammar, prettify: bool = False
+    ) -> str:
+        return _core.JSONSerializer.serialize_compiled_grammar(compiled_grammar._handle, prettify)
+
+    @staticmethod
+    def deserialize_compiled_grammar(
+        serialized_compiled_grammar: str, encoded_vocab: List[Union[bytes, str]]
+    ) -> CompiledGrammar:
+        return CompiledGrammar._create_from_handle(
+            _core.JSONSerializer.deserialize_compiled_grammar(
+                serialized_compiled_grammar, encoded_vocab
+            )
+        )
