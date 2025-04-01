ae5adf7f307f6f017d6ae5b3f33688a930cd0dd3
Yi Wang <yi.wang.2005@gmail.com>
2025-03-31 22:11:49 -0700

    Add benchmark

--- python/xgrammar/contrib/mlxlm.py
+++ python/xgrammar/contrib/mlxlm.py
@@ -4,6 +4,8 @@ Usage:
 """
 
 import argparse
+import itertools
+import timeit
 
 import mlx.core as mx
 import torch
@@ -27,9 +29,9 @@ def apply_token_bitmask_inplace_mlx(bitmask: torch.Tensor, logits: mx.array) -> 
     Returns:
         mx.array: The masked logits where invalid tokens have their logits set to -inf
     """
-    # If Metal implementation is available, use it
-    if "metal" in xgrammar.kernels.apply_token_bitmask_inplace_kernels:
-        return xgrammar.kernels.apply_token_bitmask_inplace_kernels["metal"](bitmask, logits)
+    # # If Metal implementation is available, use it
+    # if "metal" in xgrammar.kernels.apply_token_bitmask_inplace_kernels:
+    #     return xgrammar.kernels.apply_token_bitmask_inplace_kernels["metal"](bitmask, logits)
 
     # Otherwise, fall back to the naive implementation
     vocab_size = logits.shape[-1]
@@ -47,6 +49,10 @@ def apply_token_bitmask_inplace_mlx(bitmask: torch.Tensor, logits: mx.array) -> 
     return logits + mx.array(logits_mask.numpy())
 
 
+g_bitmasks = []
+g_logits = []
+
+
 class XGrammarLogitsProcessor:
     def __init__(self, grammar: xgrammar.CompiledGrammar, max_rollback_tokens: int = 16):
         self.matcher = xgrammar.GrammarMatcher(grammar, max_rollback_tokens=max_rollback_tokens)
@@ -60,7 +66,11 @@ class XGrammarLogitsProcessor:
             self.matcher.reset()
             self.matcher.accept_token(last_token)
         if not self.matcher.is_terminated():
+            global g_bitmasks
+            global g_logits
             self.matcher.fill_next_token_bitmask(self.bitmask)
+            g_bitmasks.append(self.bitmask)
+            g_logits.append(logits)
             return apply_token_bitmask_inplace_mlx(self.bitmask, logits)
         return logits
 
@@ -75,11 +85,23 @@ def parse_args():
     return parser.parse_args()
 
 
+@mx.compile
+def apply_bitmask_compiled(bitmask, logits, vocab_size):
+    bitmap = mx.array(
+        [l[::-1] for l in itertools.product(*[[float("-inf"), 0]] * 8)], dtype=logits.dtype
+    )
+    bitmask = bitmask.view(mx.uint8)
+    return logits[..., :vocab_size] + bitmap[bitmask].flatten(-2)[..., :vocab_size]
+
+
 def main():
     args = parse_args()
     model, _ = mlx_load(args.model)
     tokenizer = AutoTokenizer.from_pretrained(args.model)
     mx.random.seed(args.seed)
+
+    vocab_size = xgrammar.TokenizerInfo.from_huggingface(tokenizer).vocab_size
+
     with_logits_processor = mlx_generate(
         model=model,
         tokenizer=tokenizer,
@@ -104,6 +126,23 @@ def main():
         verbose=False,
     )
     assert without_logits_processor == with_logits_processor
+
+    global g_bitmasks
+    global g_logits
+    for i in range(min(3, len(g_logits))):
+        execution_time = timeit.timeit(
+            lambda: mx.eval(
+                apply_bitmask_compiled(
+                    mx.array(g_bitmasks[i].detach().numpy()), g_logits[i], vocab_size
+                )
+            ),
+            number=10,
+        )
+        print(f"apply_bitmask_compiled: {execution_time / 10} seconds")
+        execution_time = timeit.timeit(
+            lambda: mx.eval(apply_token_bitmask_inplace_mlx(g_bitmasks[i], g_logits[i])), number=10
+        )
+        print(f"apply_token_bitmask_inplace_mlx: {execution_time / 10} seconds")
 
 
 if __name__ == "__main__":
