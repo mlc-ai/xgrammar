59be3e710a2de7da4cac6b7ee723c20b4b56f16f
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-04-09 19:56:02 +0800

    feat:add the advance function of the compact FSM.

--- include/xgrammar/fsm.h
+++ include/xgrammar/fsm.h
@@ -28,9 +28,23 @@ struct FSMEdge {
   short min, max;
   int target;
   FSMEdge(const short& _min, const short& _max, const int& target);
+  /*!
+    \brief Check if the edge is an epsilon transition.
+  */
   bool IsEpsilon() const;
+  /*!
+    \brief Check if the edge is a rule reference.
+  */
   bool IsRuleRef() const;
+  /*!
+    \brief Get the rule id of the edge.
+    \return The rule id of the edge.
+    \throw std::runtime_error if the edge is not a rule reference.
+  */
   short GetRefRuleId() const;
+  /*!
+    \brief Check if the edge is a character range.
+  */
   bool IsCharRange() const;
 };
 class CompactFSM;
@@ -40,10 +54,24 @@ class FSM {
 
  public:
   using Edge = FSMEdge;
+  /*!
+    \brief Transform a FSM to a compact FSM.
+    \return The compact FSM.
+  */
   CompactFSM ToCompact();
+  /*!
+    \brief Advance the FSM to the next state.
+    \param from The current states.
+    \param value The input value.
+    \param result The next states, which can be seen as the result of the transition.
+    \param is_rule Whether the input value is a rule id.
+  */
   void Advance(
       const std::vector<int>& from, int value, std::vector<int>* result, bool is_rule = false
   ) const;
+  /*!
+    \brief Return a copy of the FSM.
+  */
   FSM Copy() const;
   // The interanl states are also public
   std::vector<std::vector<Edge>> edges;
@@ -56,18 +84,62 @@ class FSMWithStartEnd {
   FSM fsm;
   int start;
   std::unordered_set<int> ends;
+  /*!
+    \brief Transform the FSM to a DFA.
+    \return The DFA.
+  */
   FSMWithStartEnd TODFA() const;
+  /*!
+    \brief Transform the FSM to accept the complement of the language.
+    \return The complement FSM.
+  */
   FSMWithStartEnd Not() const;
+  /*!
+    \brief Minimize the DFA.
+    \return The minimized DFA.
+  */
   FSMWithStartEnd MinimizeDFA() const;
+  /*!
+    \brief Return a copy of the FSM.
+    \return The copy of the FSM.
+  */
   FSMWithStartEnd Copy() const;
+  /*!
+    \brief Print the FSM.
+    \return The string representation of the FSM.
+  */
   std::string Print() const;
+  /*!
+    \brief Intersect the FSMs.
+    \param lhs The left FSM.
+    \param rhs The right FSM.
+    \return The intersection of the FSMs.
+  */
   static FSMWithStartEnd Intersect(const FSMWithStartEnd& lhs, const FSMWithStartEnd& rhs);
+  /*!
+    \brief Union the FSMs.
+    \param fsms The FSMs to be unioned.
+    \return The union of the FSMs.
+  */
   static FSMWithStartEnd Union(const std::vector<FSMWithStartEnd>& fsms);
 };
 
 class CompactFSM {
  public:
-  void Advance(const std::vector<int>& from, int char_value, std::vector<int>* result) const;
+  /*!
+   \brief Advance the FSM to the next state.
+   \param from The current states.
+   \param value The input value.
+   \param result The next states, which can be seen as the result of the transition.
+   \param is_rule Whether the input value is a rule id.
+  */
+  void Advance(
+      const std::vector<int>& from, int value, std::vector<int>* result, bool is_rule = false
+  ) const;
+  /*!
+    \brief Transform the compact FSM to a FSM.
+    \return The FSM.
+  */
   FSM ToFSM();
   // The interanl states are also public
   using Edge = FSMEdge;
@@ -80,6 +152,10 @@ class CompactFSMWithStartEnd {
   CompactFSM fsm;
   int start;
   std::unordered_set<int> ends;
+  /*!
+  \brief Print the FSM.
+  \return The string representation of the FSM.
+*/
   std::string Print() const;
 };
 
