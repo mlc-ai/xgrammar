cbfc36d79b096af57e0e0ec828ffef2cf3ff7136
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-05-31 13:28:55 +0800

    feat:add a tool to test the code coverage.

---
+++ scripts/run_coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Usage: bash ./scripts/run_coverage.sh
+pytest -m "not hf_token_required"
+
+lcov --gcov-tool /usr/bin/gcov-13 --directory . --capture --output-file coverage.info
+genhtml coverage.info --output-directory coverage_report --ignore-errors version
+rm coverage.info
+echo "Coverage report generated at: $(pwd)/coverage_report/index.html"
