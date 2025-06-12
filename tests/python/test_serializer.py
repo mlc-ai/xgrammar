8f18ed2fc2556c463ea80504697a67445f030aae
DarkSharpness <2040703891@qq.com>
2025-06-12 16:04:06 -0700

    test: simplify and fix python test

--- tests/python/test_serializer.py
+++ tests/python/test_serializer.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from typing import Dict, List, Optional, Tuple
 
 import pytest
+from pydantic import BaseModel
 from transformers import AutoTokenizer  # type: ignore
 
 import xgrammar as xgr
@@ -21,38 +22,7 @@ def make_trivial_schema(num: int):
     }
 
 
-@pytest.mark.hf_token_required
-def test_serializer_basic():
-    tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH, use_fast=True, trust_remote_code=True)
-    tokenizer_info = xgr.TokenizerInfo.from_huggingface(tokenizer)
-    grammar_compiler = xgr.GrammarCompiler(tokenizer_info)
-    serializer = xgr.JSONSerializer()
-
-    schemas: List[xgr.CompiledGrammar] = []
-    for i in range(100):
-        schemas.append(grammar_compiler.compile_json_schema(make_trivial_schema(i)))
-
-    # skip the first serialization and deserialization to warm up
-    _ = serializer.deserialize_compiled_grammar(
-        serializer.serialize_compiled_grammar(schemas[-1]), tokenizer_info
-    )
-
-    # test serialization and deserialization of basic schemas
-    tic = time.monotonic_ns()
-    results: List[str] = []
-    for i in range(100):
-        results.append(serializer.serialize_compiled_grammar(schemas[i]))
-    toc = time.monotonic_ns()
-    print(f"Serialization 100 times: {(toc - tic) / 1_000_000:.2f} ms")
-
-    tic = time.monotonic_ns()
-    for i in range(100):
-        _ = serializer.deserialize_compiled_grammar(results[i], tokenizer_info)
-    toc = time.monotonic_ns()
-    print(f"Deserialization 100 times: {(toc - tic) / 1_000_000:.2f} ms")
-
-    # test serialization of EBNF grammar
-    json_grammar = r"""
+JSON_GRAMMAR = r"""
 basic_escape ::= ["\\/bfnrt] | "u" [A-Fa-f0-9] [A-Fa-f0-9] [A-Fa-f0-9] [A-Fa-f0-9]
 basic_string_sub ::= ("\"" | [^"\\\r\n] basic_string_sub | "\\" basic_escape basic_string_sub) (= [ \n\t]* [,}\]:])
 basic_any ::= basic_number | basic_string | basic_boolean | basic_null | basic_array | basic_object
@@ -72,25 +42,77 @@ root_prop_7_addl ::= ("{" "" basic_string ": " basic_integer (", " basic_string 
 root_prop_7 ::= ("{" "" basic_string ": " root_prop_7_addl (", " basic_string ": " root_prop_7_addl)* "" "}") | "{" "}"
 root ::= "{" "" "\"integer_field\"" ": " basic_integer ", " "\"number_field\"" ": " basic_number ", " "\"boolean_field\"" ": " basic_boolean ", " "\"any_array_field\"" ": " root_prop_3 ", " "\"array_field\"" ": " root_prop_4 ", " "\"tuple_field\"" ": " root_prop_5 ", " "\"object_field\"" ": " root_prop_6 ", " "\"nested_object_field\"" ": " root_prop_7 "" "}"
 """
-    grammar = grammar_compiler.compile_grammar(json_grammar)
-    tic = time.monotonic_ns()
-    serialized = serializer.serialize_compiled_grammar(grammar)
-    toc = time.monotonic_ns()
-    print(f"Serialization simple JSON EBNF: {(toc - tic) / 1_000_000:.2f} ms")
-    tic = time.monotonic_ns()
-    _ = serializer.deserialize_compiled_grammar(serialized, tokenizer_info)
-    toc = time.monotonic_ns()
-    print(f"Deserialization simple JSON EBNF: {(toc - tic) / 1_000_000:.2f} ms")
+
+EMAIL_REGEX = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}"
+
+
+class ExampleModel(BaseModel):
+    id: int
+    name: str
+
+
+EXAMPLE_TAGS = [
+    xgr.StructuralTagItem(begin="<function=f>", schema=ExampleModel, end="</function>"),
+    xgr.StructuralTagItem(begin="<function=g>", schema=ExampleModel, end="</function>"),
+]
+
+EXAMPLE_TRIGGERS = ["<function=f", "<function=g"]
+
+
+@pytest.mark.hf_token_required
+def test_serializer_roundtrip():
+    tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH, use_fast=True, trust_remote_code=True)
+    tokenizer_info = xgr.TokenizerInfo.from_huggingface(tokenizer)
+    grammar_compiler = xgr.GrammarCompiler(tokenizer_info)
+    serializer = xgr.JSONSerializer()
+
+    def _serialize_test(grammar: xgr.CompiledGrammar, name: str):
+        tic = time.monotonic_ns()
+        serialized_json = serializer.serialize_compiled_grammar(grammar)
+        toc = time.monotonic_ns()
+        print(f"Serialization {name}: {(toc - tic) / 1_000_000:.2f} ms")
+        tic = time.monotonic_ns()
+        deserialized_obj = serializer.deserialize_compiled_grammar(serialized_json, tokenizer_info)
+        toc = time.monotonic_ns()
+        print(f"Deserialization {name}: {(toc - tic) / 1_000_000:.2f} ms")
+        serialized_json_new = serializer.serialize_compiled_grammar(deserialized_obj)
+        return serialized_json == serialized_json_new
+
+    # skip the first serialization and deserialization to warm up
+    _ = serializer.deserialize_compiled_grammar(
+        serializer.serialize_compiled_grammar(grammar_compiler.compile_builtin_json_grammar()),
+        tokenizer_info,
+    )
+
+    grammar = grammar_compiler.compile_json_schema(make_trivial_schema(0))
+    _serialize_test(grammar, f"JSON schema")
+
+    grammar = grammar_compiler.compile_structural_tag(EXAMPLE_TAGS, EXAMPLE_TRIGGERS)
+    _serialize_test(grammar, "structural tag")
+
+    grammar = grammar_compiler.compile_grammar(JSON_GRAMMAR)
+    _serialize_test(grammar, "simple JSON EBNF")
 
     grammar = grammar_compiler.compile_builtin_json_grammar()
+    _serialize_test(grammar, "builtin JSON EBNF")
+
+    grammar = grammar_compiler.compile_regex(EMAIL_REGEX)
+    _serialize_test(grammar, "email regex")
+
+    # test the overall serialization time
+    schemas = [grammar_compiler.compile_json_schema(make_trivial_schema(i)) for i in range(100)]
     tic = time.monotonic_ns()
-    serialized = serializer.serialize_compiled_grammar(grammar)
+    results = [serializer.serialize_compiled_grammar(schema) for schema in schemas]
     toc = time.monotonic_ns()
-    print(f"Serialization builtin JSON: {(toc - tic) / 1_000_000:.2f} ms")
+    print(f"Serialization of 100 simple schemas: {(toc - tic) / 1_000_000:.2f} ms")
     tic = time.monotonic_ns()
-    _ = serializer.deserialize_compiled_grammar(serialized, tokenizer_info)
+    schemas_new = [
+        serializer.deserialize_compiled_grammar(result, tokenizer_info) for result in results
+    ]
     toc = time.monotonic_ns()
-    print(f"Deserialization builtin JSON: {(toc - tic) / 1_000_000:.2f} ms")
+    print(f"Deserialization of 100 simple schemas: {(toc - tic) / 1_000_000:.2f} ms")
+    results_new = [serializer.serialize_compiled_grammar(schema) for schema in schemas_new]
+    assert results == results_new, "Serialized and deserialized schemas do not match"
 
 
 tokenizer_path__input_str__expected_rejected_sizes = [
@@ -124,51 +146,16 @@ tokenizer_path__input_str__expected_rejected_sizes = [
     "tokenizer_path, input_str, expected_rejected_sizes",
     tokenizer_path__input_str__expected_rejected_sizes,
 )
-def test_serializer_correctness(
+def test_serializer_correctness_functional(
     tokenizer_path: str, input_str: str, expected_rejected_sizes: Optional[List[int]]
 ):
+    # test serialization and deserialization in practice
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, use_fast=True, trust_remote_code=True)
     tokenizer_info = xgr.TokenizerInfo.from_huggingface(tokenizer)
     grammar_compiler = xgr.GrammarCompiler(tokenizer_info)
     serializer = xgr.JSONSerializer()
 
-    schemas: List[xgr.CompiledGrammar] = []
-    for i in range(100):
-        schemas.append(grammar_compiler.compile_json_schema(make_trivial_schema(i)))
-
-    # test serialization and deserialization of basic schemas
-    # this is a hack of implementation, do not rely on this in production
-    def flatten_list(lst: List) -> List[int]:
-        result: List[int] = []
-        for item in lst:
-            if isinstance(item, list):
-                result.extend(flatten_list(item))
-            else:
-                result.append(item)
-        return result
-
-    def extract_cache(obj) -> Dict[Tuple[int, ...], List[int]]:
-        cache: List[List[int]] = obj.pop("adaptive_token_mask_cache")
-        result: Dict[Tuple[int, ...], List[int]] = defaultdict(list)
-        assert len(cache) % 2 == 0
-        for i in range(0, len(cache), 2):
-            result[tuple(cache[i])] = cache[i + 1]
-        return result
-
-    # theoretically test the correctness of serialization and deserialization
-    for i in range(100):
-        serialized = serializer.serialize_compiled_grammar(schemas[i])
-        deserialized = serializer.deserialize_compiled_grammar(serialized, tokenizer_info)
-        json_0 = json.loads(serialized)
-        json_1 = json.loads(serializer.serialize_compiled_grammar(deserialized))
-        cache_0 = extract_cache(json_0)
-        cache_1 = extract_cache(json_1)
-        assert json_0 == json_1, f"Serialization and deserialization mismatch for schema {i}"
-        assert cache_0 == cache_1, f"Cache mismatch for schema {i}"
-
-    # test serialization and deserialization in practice
     # copied from test_grammar_matcher_basic.py
-
     from xgrammar.testing import _get_masked_tokens_from_bitmask
 
     json_grammar = xgr.Grammar.builtin_json_grammar()
