f912010b1e152ce12c3518eca728fc5e7409c845
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-08-01 11:49:59 +0800

    refac the tests.

--- tests/python/test_grammar_constructor.py
+++ tests/python/test_grammar_constructor.py
@@ -39,5 +39,81 @@ def test_grammar_constructor_string(input: str, test_string: str, expected_gramm
     assert grammar_str == expected_grammar
 
 
+def test_grammar_union():
+    grammar1 = xgr.Grammar.from_ebnf(
+        """root ::= r1 | r2
+r1 ::= "true" | ""
+r2 ::= "false" | ""
+"""
+    )
+
+    grammar2 = xgr.Grammar.from_ebnf(
+        """root ::= "abc" | r1
+r1 ::= "true" | r1
+"""
+    )
+
+    grammar3 = xgr.Grammar.from_ebnf(
+        """root ::= r1 | r2 | r3
+r1 ::= "true" | r3
+r2 ::= "false" | r3
+r3 ::= "abc" | ""
+"""
+    )
+
+    expected = """root ::= ((root_1) | (root_2) | (root_3))
+root_1 ::= ((r1) | (r2))
+r1 ::= ("" | ("true"))
+r2 ::= ("" | ("false"))
+root_2 ::= (("abc") | (r1_1))
+r1_1 ::= (("true") | (r1_1))
+root_3 ::= ((r1_2) | (r2_1) | (r3))
+r1_2 ::= (("true") | (r3))
+r2_1 ::= (("false") | (r3))
+r3 ::= ("" | ("abc"))
+"""
+
+    union_grammar = xgr.Grammar.union(grammar1, grammar2, grammar3)
+    assert str(union_grammar) == expected
+
+
+def test_grammar_concat():
+    grammar1 = xgr.Grammar.from_ebnf(
+        """root ::= r1 | r2
+r1 ::= "true" | ""
+r2 ::= "false" | ""
+"""
+    )
+
+    grammar2 = xgr.Grammar.from_ebnf(
+        """root ::= "abc" | r1
+r1 ::= "true" | r1
+"""
+    )
+
+    grammar3 = xgr.Grammar.from_ebnf(
+        """root ::= r1 | r2 | r3
+r1 ::= "true" | r3
+r2 ::= "false" | r3
+r3 ::= "abc" | ""
+"""
+    )
+
+    expected = """root ::= ((root_1 root_2 root_3))
+root_1 ::= ((r1) | (r2))
+r1 ::= ("" | ("true"))
+r2 ::= ("" | ("false"))
+root_2 ::= (("abc") | (r1_1))
+r1_1 ::= (("true") | (r1_1))
+root_3 ::= ((r1_2) | (r2_1) | (r3))
+r1_2 ::= (("true") | (r3))
+r2_1 ::= (("false") | (r3))
+r3 ::= ("" | ("abc"))
+"""
+
+    concat_grammar = xgr.Grammar.concat(grammar1, grammar2, grammar3)
+    assert str(concat_grammar) == expected
+
+
 if __name__ == "__main__":
     pytest.main(sys.argv)
