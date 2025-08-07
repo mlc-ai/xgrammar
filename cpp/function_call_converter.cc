469ff7a300fede2fa835d573af00cd7718c476b1
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-08-07 16:02:30 +0800

    refac the design.

--- cpp/function_call_converter.cc
+++ cpp/function_call_converter.cc
@@ -16,130 +16,33 @@
 
 namespace xgrammar {
 
-class FunctionCallConverterImpl {
- public:
-  enum class kParametersType { kNumber = 0, kString = 1, kBoolean = 2, kObject = 3 };
-
-  static Grammar Apply(
-      const std::vector<std::string>& args_names,
-      const std::vector<std::string>& args_types,
-      uint8_t function_type
-  );
-
-  static Grammar BuildXmlParameterGrammar(const std::string& arg_name, const std::string& arg_type);
-
-  static Grammar DecorateXmlParameterGrammar(std::vector<Grammar>& grammar);
-};
-
-Grammar FunctionCallConverterImpl::Apply(
-    const std::vector<std::string>& args_names,
-    const std::vector<std::string>& args_types,
-    uint8_t function_type
-) {
-  // Handle the corner cases.
-  XGRAMMAR_CHECK(args_types.size() == args_types.size());
-  if (args_names.empty()) {
-    return Grammar::FromEBNF("root ::= \"\"");
-  }
-
-  // Build the single grammars.
-  std::vector<Grammar> result_grammar;
-  for (int i = 0; i < static_cast<int>(args_names.size()); i++) {
-    switch (function_type) {
-      case Grammar::kXmlStyleFunctionCall: {
-        result_grammar.push_back(BuildXmlParameterGrammar(args_names[i], args_types[i]));
-        break;
-      }
-      default: {
-        XGRAMMAR_LOG(FATAL) << "Can't build a function calling grammar for this style!";
-      }
-    }
-  }
-
-  // concat and decorate the grammars.
-  switch (function_type) {
-    case Grammar::kXmlStyleFunctionCall: {
-      return DecorateXmlParameterGrammar(result_grammar);
-    }
-    default: {
-      XGRAMMAR_LOG(FATAL) << "Can't build a function calling grammar for this style!";
-    }
-  }
-}
-
-Grammar FunctionCallConverterImpl::BuildXmlParameterGrammar(
-    const std::string& arg_name, const std::string& arg_type
-) {
-  static const std::unordered_map<std::string, kParametersType> raw_string_to_types = {
-      {"string", kParametersType::kString},
-      {"str", kParametersType::kString},
-      {"char", kParametersType::kString},
-      {"enum", kParametersType::kString},
-      {"text", kParametersType::kString},
-      {"varchar", kParametersType::kString},
-      {"int", kParametersType::kNumber},
-      {"uint", kParametersType::kNumber},
-      {"long", kParametersType::kNumber},
-      {"short", kParametersType::kNumber},
-      {"unsign", kParametersType::kNumber},
-      {"float", kParametersType::kNumber},
-      {"num", kParametersType::kNumber},
-      {"boolean", kParametersType::kBoolean},
-      {"bool", kParametersType::kBoolean},
-      {"binary", kParametersType::kBoolean},
-      {"object", kParametersType::kObject},
-      {"dict", kParametersType::kObject}
-  };
-
-  // Decode the arg type.
-  kParametersType decoded_arg_type = kParametersType::kString;
-  if (raw_string_to_types.find(arg_type) != raw_string_to_types.end()) {
-    decoded_arg_type = raw_string_to_types.at(arg_type);
-  } else {
-    // Check if the arg_type is started with some types. Otherwise, use strings.
-    for (auto& [raw_string, type] : raw_string_to_types) {
-      if (arg_type.size() >= raw_string.size() &&
-          arg_type.substr(0, raw_string.size()) == raw_string) {
-        decoded_arg_type = type;
-        break;
-      }
-    }
-  }
-  XGRAMMAR_UNREACHABLE();
-}
-
-/*************************** Forward grammar functors to their impl ***************************/
-
-Grammar FunctionCallConverter::Apply(
-    const std::vector<std::string>& args_names,
-    const std::vector<std::string>& args_types,
-    uint8_t function_type
-) {
-  return FunctionCallConverterImpl::Apply(args_names, args_types, function_type);
-}
-
 /*************************** Grammar definitions ***************************/
 
+const std::string kWhiteSpacesString = R"( [ \n\t]* )";
+
 const std::string kNumberGrammarString = R"(
-root ::= sign "0" fraction exponent | sign [1-9] [0-9]* fraction exponent
+number ::= sign "0" fraction exponent [ \n\t]* "</parameter>" |
+    sign [1-9] [0-9]* fraction exponent [ \n\t]* "</parameter>"
 fraction ::= "" | "." [0-9] [0-9]*
 exponent ::= "" |  "e" sign [0-9] [0-9]* | "E" sign [0-9] [0-9]*
 sign ::= "" | "+" | "-"
 )";
 
-const std::string kStringGrammarString = R"(
-root ::= [^\\\x00-\x1F] | [\\] escape
+const std::string kXmlStringGrammarString = R"(
+string ::= xml_content [ \n\t]* "</parameter>"
+xml_content ::= ([^<>&\\\x00-\x1F] | xml_entity | [\\] escape)*
+xml_entity ::= "&lt;" | "&gt;" | "&amp;" | "&quot;" | "&apos;"
 escape ::= ["\\/bfnrt] | "u" [A-Fa-f0-9] [A-Fa-f0-9] [A-Fa-f0-9] [A-Fa-f0-9]
 )";
 
 const std::string kBooleanGrammarString = R"(
-root ::= "true" | "false"
+boolean ::= "true" [ \n\t]* "</parameter>" | "false" [ \n\t]* "</parameter>"
 )";
 
-const std::string kObjectGrammarString = R"(
-root ::= (
-    "{" [ \n\t]* members_and_embrace | "null"
-)
+const std::string kXmlObjectGrammarString = R"(
+object ::=
+    "{" [ \n\t]* members_and_embrace [ \n\t]* "</parameter>" | "null" [ \n\t]* "</parameter>"
+xml_entity ::= "&lt;" | "&gt;" | "&amp;" | "&quot;" | "&apos;"
 value_non_str ::= (
     "{" [ \n\t]* members_and_embrace |
     "[" [ \n\t]* elements_or_embrace |
@@ -192,22 +95,26 @@ elements_rest ::= (
 )
 characters_and_colon ::= (
     "\"" [ \n\t]* ":" |
-    [^"\\\x00-\x1F] characters_and_colon |
+    [^<>&\\\x00-\x1F] characters_and_colon |
+    xml_entity characters_and_colon |
     "\\" escape characters_and_colon
 ) (=[ \n\t]* [\"{[0-9tfn-])
 characters_and_comma ::= (
     "\"" [ \n\t]* "," |
-    [^"\\\x00-\x1F] characters_and_comma |
+    [^<>&\\\x00-\x1F] characters_and_comma |
+    xml_entity characters_and_comma |
     "\\" escape characters_and_comma
 ) (=[ \n\t]* "\"")
 characters_and_embrace ::= (
     "\"" [ \n\t]* "}" |
-    [^"\\\x00-\x1F] characters_and_embrace |
+    [^<>&\\\x00-\x1F] characters_and_embrace |
+    xml_entity characters_and_embrace |
     "\\" escape characters_and_embrace
 ) (=[ \n\t]* [},])
 characters_item ::= (
     "\"" |
-    [^"\\\x00-\x1F] characters_item |
+    [^<>&\\\x00-\x1F] characters_item |
+    xml_entity characters_item |
     "\\" escape characters_item
 ) (= [ \n\t]* [,\]])
 escape ::= ["\\/bfnrt] | "u" [A-Fa-f0-9] [A-Fa-f0-9] [A-Fa-f0-9] [A-Fa-f0-9]
@@ -216,4 +123,91 @@ exponent ::= "" |  "e" sign [0-9] [0-9]* | "E" sign [0-9] [0-9]*
 sign ::= "" | "+" | "-"
 )";
 
+/*************************** FunctionCallConverterImpl ***************************/
+
+class FunctionCallConverterImpl {
+ public:
+  enum class kParametersType : int32_t { kNumber = 0, kString = 1, kBoolean = 2, kObject = 3 };
+
+  static Grammar Apply(
+      const std::vector<std::string>& args_names,
+      const std::vector<std::string>& args_types,
+      uint8_t function_type
+  );
+
+  static Grammar BuildXmlParameterGrammar(
+      const std::vector<std::string>& args_names, const std::vector<std::string>& args_types
+  );
+
+  static Grammar DecorateXmlParameterGrammar(std::vector<Grammar>& grammar);
+};
+
+Grammar FunctionCallConverterImpl::Apply(
+    const std::vector<std::string>& args_names,
+    const std::vector<std::string>& args_types,
+    uint8_t function_type
+) {
+  // Handle the corner cases.
+  XGRAMMAR_CHECK(args_types.size() == args_types.size());
+  if (args_names.empty()) {
+    return Grammar::FromEBNF("root ::= \"\"");
+  }
+
+  // Build the grammar
+  switch (function_type) {
+    case Grammar::kXmlStyleFunctionCall: {
+      return BuildXmlParameterGrammar(args_names, args_types);
+    }
+    default: {
+      XGRAMMAR_LOG(FATAL) << "Can't build a function calling grammar for this style!";
+    }
+  }
+}
+
+using kParametersType = FunctionCallConverterImpl::kParametersType;
+
+static const std::unordered_map<std::string, kParametersType> raw_string_to_types = {
+    {"string", kParametersType::kString},
+    {"str", kParametersType::kString},
+    {"char", kParametersType::kString},
+    {"enum", kParametersType::kString},
+    {"text", kParametersType::kString},
+    {"varchar", kParametersType::kString},
+    {"int", kParametersType::kNumber},
+    {"uint", kParametersType::kNumber},
+    {"long", kParametersType::kNumber},
+    {"short", kParametersType::kNumber},
+    {"unsign", kParametersType::kNumber},
+    {"float", kParametersType::kNumber},
+    {"num", kParametersType::kNumber},
+    {"boolean", kParametersType::kBoolean},
+    {"bool", kParametersType::kBoolean},
+    {"binary", kParametersType::kBoolean},
+    {"object", kParametersType::kObject},
+    {"dict", kParametersType::kObject}
+};
+
+static const Grammar kXmlStringGrammar = Grammar::FromEBNF(kXmlStringGrammarString, "string");
+
+static const Grammar kNumberGrammar = Grammar::FromEBNF(kNumberGrammarString, "number");
+
+static const Grammar kBooleanGrammar = Grammar::FromEBNF(kBooleanGrammarString, "boolean");
+
+static const Grammar kXmlObjectGrammar = Grammar::FromEBNF(kXmlObjectGrammarString, "object");
+
+Grammar FunctionCallConverterImpl::BuildXmlParameterGrammar(
+    const std::vector<std::string>& arg_names, const std::vector<std::string>& arg_types
+) {
+  XGRAMMAR_UNREACHABLE();
+}
+
+/*************************** Forward grammar functors to their impl ***************************/
+
+Grammar FunctionCallConverter::Apply(
+    const std::vector<std::string>& args_names,
+    const std::vector<std::string>& args_types,
+    uint8_t function_type
+) {
+  return FunctionCallConverterImpl::Apply(args_names, args_types, function_type);
+}
 }  // namespace xgrammar
