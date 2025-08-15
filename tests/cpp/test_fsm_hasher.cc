5e6e07e92da5d6dd118c99d0bcef60953cd8b279
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-08-15 16:39:27 +0800

    test:add more hasher tests.
    Signed-off-by: Yuchuan <blemiade_qinchuan@sjtu.edu.cn>

---
+++ tests/cpp/test_fsm_hasher.cc
@@ -0,0 +1,77 @@
+/**
+ * \file tests/cpp/test_fsm_hasher.cc
+ * \brief Test the FSM hasher.
+ */
+
+#include <gtest/gtest.h>
+#include <xgrammar/xgrammar.h>
+
+#include <optional>
+
+#include "grammar_functor.h"
+#include "xgrammar/grammar.h"
+
+using namespace xgrammar;
+
+TEST(XGrammarFSMHasherTest, TestTerminalfsm) {
+  Grammar grammar[2] = {
+      Grammar::FromEBNF("root ::= [a-z] | [0-9]"), Grammar::FromEBNF("root ::= [0-9] | [a-z]")
+  };
+  GrammarFSMBuilder::Apply(&grammar[0]);
+  GrammarFSMBuilder::Apply(&grammar[1]);
+  GrammarFSMHasher::Apply(&grammar[0]);
+  GrammarFSMHasher::Apply(&grammar[1]);
+  EXPECT_EQ(grammar[0]->per_rule_fsm_hashes[0], grammar[1]->per_rule_fsm_hashes[0]);
+}
+
+TEST(XGrammarFSMHasherTest, TestNonTerminalAndRecursionFsm) {
+  Grammar grammar[2] = {
+      Grammar::FromEBNF("root ::= root1 | [a-z]\n root1 ::= [0-9] root1"),
+      Grammar::FromEBNF("root1 ::= [0-9] root1\n root ::= root1 | [a-z]")
+  };
+  GrammarFSMBuilder::Apply(&grammar[0]);
+  GrammarFSMBuilder::Apply(&grammar[1]);
+  GrammarFSMHasher::Apply(&grammar[0]);
+  GrammarFSMHasher::Apply(&grammar[1]);
+  EXPECT_EQ(grammar[0]->per_rule_fsm_hashes[0], grammar[1]->per_rule_fsm_hashes[1]);
+  EXPECT_EQ(grammar[0]->per_rule_fsm_hashes[1], grammar[1]->per_rule_fsm_hashes[0]);
+}
+
+TEST(XGrammarFSMHasherTest, TestLoopReferenceFsm) {
+  std::string ebnf_grammar = R"(
+    root ::= rule1 | rule3
+    rule1 ::= [0-9] rule2
+    rule2 ::= [A-Z] | rule1
+    rule3 ::= [0-9] rule3
+    )";
+  auto grammar = Grammar::FromEBNF(ebnf_grammar);
+  GrammarFSMBuilder::Apply(&grammar);
+  GrammarFSMHasher::Apply(&grammar);
+  EXPECT_EQ(grammar->per_rule_fsm_hashes[0], std::nullopt);
+  EXPECT_EQ(grammar->per_rule_fsm_hashes[1], std::nullopt);
+  EXPECT_EQ(grammar->per_rule_fsm_hashes[2], std::nullopt);
+  EXPECT_EQ(grammar->per_rule_fsm_hashes[3], -1269379148);
+}
+
+TEST(XGrammarFSMHasherTest, TestComplexFsm) {
+  std::string ebnf_grammar[2] = {
+      R"(
+    root ::= "" | root1 | root | [a-z]
+    root1 ::= [0-9] | [A-Z] | root2 | ""
+    root2 ::= "" | "testing"
+)",
+      R"(
+    root2 ::= "testing" | ""
+    root ::= [a-z] | root1 | root | ""
+    root1 ::=  root2 | "" | [0-9] | [A-Z]
+)"
+  };
+  Grammar grammar[2] = {Grammar::FromEBNF(ebnf_grammar[0]), Grammar::FromEBNF(ebnf_grammar[1])};
+  GrammarFSMBuilder::Apply(&grammar[0]);
+  GrammarFSMHasher::Apply(&grammar[0]);
+  GrammarFSMBuilder::Apply(&grammar[1]);
+  GrammarFSMHasher::Apply(&grammar[1]);
+  EXPECT_EQ(grammar[0]->per_rule_fsm_hashes[0], grammar[1]->per_rule_fsm_hashes[1]);
+  EXPECT_EQ(grammar[0]->per_rule_fsm_hashes[1], grammar[1]->per_rule_fsm_hashes[2]);
+  EXPECT_EQ(grammar[0]->per_rule_fsm_hashes[2], grammar[1]->per_rule_fsm_hashes[0]);
+}
