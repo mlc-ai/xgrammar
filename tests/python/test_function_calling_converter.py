baf4948849c8251277121235bea83a31e0e429de
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-08-31 17:14:36 +0800

    format.

--- tests/python/test_function_calling_converter.py
+++ tests/python/test_function_calling_converter.py
@@ -362,7 +362,6 @@ root ::=  [ \n\t]* (("<parameter=name>" [ \n\t]* root_prop_0 [ \n\t]* "</paramet
     }
 
     ebnf_grammar = _qwen_xml_tool_calling_to_ebnf(schema)
-    print(ebnf_grammar)
     assert str(ebnf_grammar[:-2]) == expected_grammar
     assert _is_grammar_accept_string(ebnf_grammar, input_str) == accepted
 
