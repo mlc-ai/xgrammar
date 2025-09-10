mlc-ai/xgrammar

commit 8d68545efa57a32b3df1c156d6b5aa9879bf7bf0

[Refactor] JSON serializer and MemorySize  (#380)
A rebased version of #356.

---------

Co-authored-by: Ubospica <ubospica@gmail.com>

3rdparty/picojson/picojson.h
@@ -497,9 +497,10 @@ GET(std::string, *u_.string_)
 GET(array, *u_.array_)
 GET(object, *u_.object_)
 #ifdef PICOJSON_USE_INT64
-GET(double, (type_ == int64_type && (const_cast<value*>(this)->type_ = number_type,
-                                     (const_cast<value*>(this)->u_.number_ = u_.int64_)),
-             u_.number_))
+GET(double,
+    (type_ == int64_type && (const_cast<value*>(this)->type_ = number_type,
+                             (const_cast<value*>(this)->u_.number_ = u_.int64_)),
+     u_.number_))
 GET(int64_t, u_.int64_)
 #else
 GET(double, u_.number_)
@@ -609,9 +610,12 @@ inline std::string value::to_str() const {
     case number_type: {
       char buf[256];
       double tmp;
-      SNPRINTF(buf, sizeof(buf),
-               fabs(u_.number_) < (1ULL << 53) && modf(u_.number_, &tmp) == 0 ? "%.f" : "%.17g",
-               u_.number_);
+      SNPRINTF(
+          buf,
+          sizeof(buf),
+          fabs(u_.number_) < (1ULL << 53) && modf(u_.number_, &tmp) == 0 ? "%.f" : "%.17g",
+          u_.number_
+      );
 #if PICOJSON_USE_LOCALE
       char* decimal_point = localeconv()->decimal_point;
       if (strcmp(decimal_point, ".") != 0) {
@@ -664,9 +668,9 @@ struct serialize_str_char {
       MAP('\t', "\\t");
 #undef MAP
       default:
-        if (static_cast<unsigned char>(c) < 0x20 || c == 0x7f) {
+        if (static_cast<unsigned char>(c) < 0x20 || static_cast<unsigned char>(c) >= 0x7f) {
           char buf[7];
-          SNPRINTF(buf, sizeof(buf), "\\u%04x", c & 0xff);
+          SNPRINTF(buf, sizeof(buf), "\\u%04x", static_cast<unsigned char>(c));
           copy(buf, buf + 6, oi);
         } else {
           *oi++ = c;

cpp/support/encoding.h
@@ -7,66 +7,19 @@
 #define XGRAMMAR_SUPPORT_ENCODING_H_
 // TODO(yixin): enhance performance
 
+#include <array>
 #include <cstdint>
+#include <optional>
 #include <string>
 #include <unordered_map>
 #include <vector>
+
+#include "logging.h"
 
 namespace xgrammar {
 
 /*! \brief Represents a unicode codepoint. */
 using TCodepoint = int32_t;
-
-/*!
- * \brief Handle the utf-8 first byte.
- * \returns (is_valid, total_number_of_bytes, initial_codepoint).
- */
-std::tuple<bool, int, TCodepoint> HandleUTF8FirstByte(uint8_t byte);
-
-/*!
- * \brief Print a codepoint to a UTF-8 string.
- * \param codepoint The codepoint.
- * \return The UTF-8 string.
- */
-std::string PrintAsUTF8(TCodepoint codepoint);
-
-/*!
- * \brief Print a codepoint to a escaped string. If the codepoint is not printable, it will be
- * escaped. By default the function support escape sequences in C ("\n", "\t", "\u0123"). User can
- * specify more escape sequences using additional_escape_map.
- * \param codepoint The codepoint.
- * \param additional_escape_map A map from codepoint to escape sequence. If the codepoint is in the
- * map, it will be escaped using the corresponding escape sequence. e.g. {{'-', "\\-"}}. \return The
- * printable string.
- */
-std::string PrintAsEscapedUTF8(
-    TCodepoint codepoint,
-    const std::unordered_map<TCodepoint, std::string>& additional_escape_map = {}
-);
-
-/*!
- * \brief Print the given char to a escaped string that can be printed.
- * \return The escaped string.
- */
-std::string PrintAsEscapedUTF8(uint8_t raw_char);
-
-/*!
- * \brief Print the given string to a escaped string that can be printed.
- * \return The escaped string.
- */
-std::string PrintAsEscapedUTF8(std::string raw_str);
-
-inline int HexCharToInt(char c) {
-  if (c >= '0' && c <= '9') {
-    return c - '0';
-  } else if (c >= 'a' && c <= 'f') {
-    return c - 'a' + 10;
-  } else if (c >= 'A' && c <= 'F') {
-    return c - 'A' + 10;
-  } else {
-    return -1;
-  }
-}
 
 /*!
  * \brief Represents an error when handling characters. Will be returned as a special TCodepoint
@@ -77,7 +30,24 @@ enum CharHandlingError : TCodepoint {
   kInvalidUTF8 = -10,
   /*! \brief The escape sequence is invalid. */
   kInvalidEscape = -11,
+  /*! \brief The Latin-1 string is invalid. */
+  kInvalidLatin1 = -12,
 };
+
+/******************** UTF-8 Handling ********************/
+
+/*!
+ * \brief Print a codepoint to a UTF-8 string.
+ * \param codepoint The codepoint.
+ * \return The UTF-8 string.
+ */
+std::string CharToUTF8(TCodepoint codepoint);
+
+/*!
+ * \brief Handle the utf-8 first byte.
+ * \returns (is_valid, total_number_of_bytes, initial_codepoint).
+ */
+std::tuple<bool, int, TCodepoint> HandleUTF8FirstByte(uint8_t byte);
 
 /*!
  * \brief Parse all codepoints in a UTF-8 string.
@@ -98,6 +68,62 @@ std::vector<TCodepoint> ParseUTF8(const char* utf8, bool perserve_invalid_bytes 
 std::pair<TCodepoint, int32_t> ParseNextUTF8(const char* utf8);
 
 /*!
+ * \brief Convert a Latin-1 string to a byte sequence.
+ * \param latin1 The Latin-1 string.
+ * \return The byte sequence.
+ */
+std::optional<CharHandlingError> Latin1ToBytes(const std::string& latin1, std::string* result);
+
+/******************** Escape Handling ********************/
+
+/*!
+ * \brief Convert a codepoint to a escaped string. If the codepoint is not printable, it will be
+ * escaped. By default the function support escape sequences in C ("\n", "\t", "\u0123"). User
+ * can specify more escape sequences using additional_escape_map.
+ * \param codepoint The codepoint.
+ * \param additional_escape_map A map from codepoint to escape sequence. If the codepoint is in
+ * the map, it will be escaped using the corresponding escape sequence. e.g. {{'-', "\\-"}}.
+ * \return The printable string.
+ */
+std::string EscapeString(
+    TCodepoint codepoint,
+    const std::unordered_map<TCodepoint, std::string>& additional_escape_map = {}
+);
+
+/*!
+ * \brief Convert the given char to a escaped string that can be printed.
+ * \return The escaped string.
+ */
+std::string EscapeString(uint8_t raw_char);
+
+/*!
+ * \brief Convert the given string to a escaped string that can be printed.
+ * \return The escaped string.
+ */
+std::string EscapeString(std::string raw_str);
+
+/*!
+ * \brief Convert a hex character to an integer.
+ * \param c The hex character: 0-9, a-f, A-F.
+ * \return The integer value of the hex character. If the character is not a valid hex character,
+ * return -1.
+ */
+int HexCharToInt(char c);
+
+/*!
+ * \brief Parse the first escaped codepoint from a escaped string. data must start with a '\'
+ * character.
+ * \param data The escaped string. Can be TCodepoint* (e.g. string decoded from UTF-8) or char*.
+ * \param additional_escape_map A map from escape sequence to codepoint. If the escape sequence is
+ * in the map, it will be converted to the corresponding codepoint. e.g. {{"\\-", '-'}}.
+ * \return The codepoint and the number of bytes consumed.
+ */
+template <typename CharType>
+std::pair<TCodepoint, int32_t> ParseNextEscaped(
+    const CharType* data, const std::unordered_map<char, TCodepoint>& additional_escape_map = {}
+);
+
+/*!
  * \brief Parse the first codepoint from a UTF-8 string. Also checks escape sequences and converts
  * the escaped char to its original value.
  * \param utf8 The UTF-8 string or the escape sequence.
@@ -111,16 +137,204 @@ std::pair<TCodepoint, int32_t> ParseNextUTF8OrEscaped(
     const char* utf8, const std::unordered_map<char, TCodepoint>& additional_escape_map = {}
 );
 
-/*!
- * \brief Parse the first codepoint from a escaped string. It doesn't consider the UTF-8 sequences.
- * \param data The escaped string. Can be TCodepoint* (e.g. string decoded from UTF-8) or char*.
- * \param additional_escape_map A map from escape sequence to codepoint. If the escape sequence is
- * in the map, it will be converted to the corresponding codepoint. e.g. {{"\\-", '-'}}.
- * \return The codepoint and the number of bytes consumed.
- */
+/******************** Implementation ********************/
+
+inline std::string CharToUTF8(TCodepoint codepoint) {
+  XGRAMMAR_DCHECK(codepoint <= 0x10FFFF) << "Invalid codepoint: " << codepoint;
+  std::string utf8;
+  if (codepoint <= 0x7F) {
+    // 1-byte sequence
+    utf8 += static_cast<char>(codepoint);
+  } else if (codepoint <= 0x7FF) {
+    // 2-byte sequence
+    utf8 += static_cast<char>(0xC0 | ((codepoint >> 6) & 0x1F));
+    utf8 += static_cast<char>(0x80 | (codepoint & 0x3F));
+  } else if (codepoint <= 0xFFFF) {
+    // 3-byte sequence
+    utf8 += static_cast<char>(0xE0 | ((codepoint >> 12) & 0x0F));
+    utf8 += static_cast<char>(0x80 | ((codepoint >> 6) & 0x3F));
+    utf8 += static_cast<char>(0x80 | (codepoint & 0x3F));
+  } else {
+    // 4-byte sequence
+    utf8 += static_cast<char>(0xF0 | ((codepoint >> 18) & 0x07));
+    utf8 += static_cast<char>(0x80 | ((codepoint >> 12) & 0x3F));
+    utf8 += static_cast<char>(0x80 | ((codepoint >> 6) & 0x3F));
+    utf8 += static_cast<char>(0x80 | (codepoint & 0x3F));
+  }
+  return utf8;
+}
+
+inline std::tuple<bool, int, TCodepoint> HandleUTF8FirstByte(uint8_t byte) {
+  static const std::array<int8_t, 5> kFirstByteMask = {0x00, 0x7F, 0x1F, 0x0F, 0x07};
+  // clang-format off
+  static const std::array<int, 256> kUtf8Bytes = {
+     1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+     1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+     1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+     1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+     1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+     1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+     1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+     1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+     2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
+     2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
+     3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,
+    4,  4,  4,  4,  4,  4,  4,  4, -1, -1, -1, -1, -1, -1, -1, -1,
+  };
+  // clang-format on
+  auto num_bytes = kUtf8Bytes[static_cast<uint8_t>(byte)];
+  if (num_bytes == -1) {
+    return {false, 0, 0};
+  }
+  return {true, num_bytes, byte & kFirstByteMask[num_bytes]};
+}
+
+inline std::pair<TCodepoint, int32_t> ParseNextUTF8(const char* utf8) {
+  auto [accepted, num_bytes, res] = HandleUTF8FirstByte(utf8[0]);
+  if (accepted) {
+    for (int i = 1; i < num_bytes; ++i) {
+      if (utf8[i] == 0 || (static_cast<uint8_t>(utf8[i]) & 0xC0) != 0x80) {
+        // invalid utf8
+        accepted = false;
+        break;
+      }
+      res = (res << 6) | (static_cast<uint8_t>(utf8[i]) & 0x3F);
+    }
+  }
+
+  if (!accepted) {
+    // invalid utf8
+    return {CharHandlingError::kInvalidUTF8, 0};
+  }
+
+  return {res, num_bytes};
+}
+
+inline std::vector<TCodepoint> ParseUTF8(const char* utf8, bool perserve_invalid_bytes) {
+  std::vector<TCodepoint> codepoints;
+  while (*utf8 != 0) {
+    auto [codepoint, num_bytes] = ParseNextUTF8(utf8);
+    if (codepoint == CharHandlingError::kInvalidUTF8) {
+      if (perserve_invalid_bytes) {
+        codepoints.push_back(static_cast<TCodepoint>(static_cast<uint8_t>(utf8[0])));
+        utf8 += 1;
+        continue;
+      } else {
+        return {CharHandlingError::kInvalidUTF8};
+      }
+    }
+    codepoints.push_back(codepoint);
+    utf8 += num_bytes;
+  }
+  return codepoints;
+}
+
+inline std::optional<CharHandlingError> Latin1ToBytes(
+    const std::string& latin1, std::string* result
+) {
+  result->clear();
+  result->reserve(latin1.size());
+
+  const size_t len = latin1.size();
+  for (size_t i = 0; i < len; ++i) {
+    unsigned char c1 = static_cast<unsigned char>(latin1[i]);
+    if (c1 < 0x80) {
+      result->push_back(static_cast<char>(c1));
+    } else {
+      if (i + 1 >= len) {
+        return CharHandlingError::kInvalidLatin1;
+      }
+
+      unsigned char c2 = static_cast<unsigned char>(latin1[i + 1]);
+      if ((c2 & 0xC0) != 0x80) {
+        return CharHandlingError::kInvalidLatin1;
+      }
+
+      int code = ((c1 & 0x1F) << 6) | (c2 & 0x3F);
+      if (code < 0x80 || code > 0xFF) {
+        return CharHandlingError::kInvalidLatin1;
+      }
+
+      result->push_back(static_cast<char>(code));
+      ++i;
+    }
+  }
+
+  return std::nullopt;
+}
+
+inline int HexCharToInt(char c) {
+  if (c >= '0' && c <= '9') {
+    return c - '0';
+  } else if (c >= 'a' && c <= 'f') {
+    return c - 'a' + 10;
+  } else if (c >= 'A' && c <= 'F') {
+    return c - 'A' + 10;
+  } else {
+    return -1;
+  }
+}
+
+inline std::string EscapeString(
+    TCodepoint codepoint, const std::unordered_map<TCodepoint, std::string>& additional_escape_map
+) {
+  static const std::unordered_map<TCodepoint, std::string> kCodepointToEscape = {
+      {'\'', "\\\'"},
+      {'\"', "\\\""},
+      {'\?', "\\?"},
+      {'\\', "\\\\"},
+      {'\a', "\\a"},
+      {'\b', "\\b"},
+      {'\f', "\\f"},
+      {'\n', "\\n"},
+      {'\r', "\\r"},
+      {'\t', "\\t"},
+      {'\v', "\\v"},
+      {'\0', "\\0"},
+      {'\x1B', "\\e"}
+  };
+
+  if (auto it = additional_escape_map.find(codepoint); it != additional_escape_map.end()) {
+    return it->second;
+  }
+
+  if (auto it = kCodepointToEscape.find(codepoint); it != kCodepointToEscape.end()) {
+    return it->second;
+  }
+
+  if (codepoint >= 0x20 && codepoint <= 0x7E) {
+    return std::string({static_cast<char>(codepoint)});
+  }
+
+  // convert codepoint to hex
+  char prefix = codepoint <= 0xFF ? 'x' : codepoint <= 0xFFFF ? 'u' : 'U';
+  int width = codepoint <= 0xFF ? 2 : codepoint <= 0xFFFF ? 4 : 8;
+  std::stringstream ss;
+  ss << std::setfill('0') << std::setw(width) << std::hex << codepoint;
+  auto hex = ss.str();
+  return std::string("\\") + prefix + hex;
+}
+
+inline std::string EscapeString(uint8_t raw_char) {
+  return EscapeString(static_cast<TCodepoint>(raw_char));
+}
+
+inline std::string EscapeString(std::string raw_str) {
+  std::string res;
+  auto codepoints = ParseUTF8(raw_str.c_str(), true);
+  for (auto c : codepoints) {
+    res += EscapeString(c);
+  }
+  return res;
+}
+
 template <typename CharType>
 std::pair<TCodepoint, int32_t> ParseNextEscaped(
-    const CharType* data, const std::unordered_map<char, TCodepoint>& additional_escape_map = {}
+    const CharType* data, const std::unordered_map<char, TCodepoint>& additional_escape_map
 ) {
   // C escape characters
   static const std::unordered_map<char, TCodepoint> kEscapeToCodepoint = {
@@ -177,6 +391,15 @@ std::pair<TCodepoint, int32_t> ParseNextEscaped(
   } else {
     return {CharHandlingError::kInvalidEscape, 0};
   }
+}
+
+inline std::pair<TCodepoint, int32_t> ParseNextUTF8OrEscaped(
+    const char* utf8, const std::unordered_map<char, TCodepoint>& additional_escape_map
+) {
+  if (utf8[0] != '\\') {
+    return ParseNextUTF8(utf8);
+  }
+  return ParseNextEscaped(utf8, additional_escape_map);
 }
 
 }  // namespace xgrammar

commit 8e2ddee4993e09d56b3cfa6d918c4e723742a9d9

[Fix] Fix the matching for utf-8 characters in enums and const. (#419)
As reported in #418, utf-8 characters will be interpreted incorrectly in
`enum` and `const`. It is caused by #380 updated the serialization
method, which is used during jsonschema conversion. This PR fixes the
bug by refactoring the serialization method, and rollback the
`picojson.h`

---------

Signed-off-by: Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
Co-authored-by: Yixin Dong <ubospica@gmail.com>

3rdparty/picojson/picojson.h
@@ -668,9 +668,9 @@ struct serialize_str_char {
       MAP('\t', "\\t");
 #undef MAP
       default:
-        if (static_cast<unsigned char>(c) < 0x20 || static_cast<unsigned char>(c) >= 0x7f) {
+        if (static_cast<unsigned char>(c) < 0x20 || c == 0x7f) {
           char buf[7];
-          SNPRINTF(buf, sizeof(buf), "\\u%04x", static_cast<unsigned char>(c));
+          SNPRINTF(buf, sizeof(buf), "\\u%04x", c & 0xff);
           copy(buf, buf + 6, oi);
         } else {
           *oi++ = c;

cpp/support/encoding.h
@@ -9,6 +9,7 @@
 
 #include <array>
 #include <cstdint>
+#include <cstdio>
 #include <optional>
 #include <string>
 #include <unordered_map>
@@ -233,6 +234,21 @@ inline std::vector<TCodepoint> ParseUTF8(const char* utf8, bool perserve_invalid
   return codepoints;
 }
 
+/*!
+  \brief Convert a Latin-1 string to a byte sequence.
+  \param latin1 The Latin-1 string.
+  \param result The output byte sequence.
+  The function will convert each Latin-1 character to its corresponding byte(s).
+  For characters in the range [0x00, 0x7F], the corresponding byte is the same as the character.
+  Otherwise, the character should be encoded in two bytes in UTF-8:
+    - First byte:  110xxxxx (0xC0 | (char >> 6))
+    - Second byte: 10xxxxxx (0x80 | (char & 0x3F))
+  Example:
+    0xC3 0xBF -> 0xFF
+    'A' -> 'A'
+  \return std::nullopt if the conversion is successful. Otherwise, return
+  CharHandlingError::kInvalidLatin1 if the Latin-1 string is invalid.
+*/
 inline std::optional<CharHandlingError> Latin1ToBytes(
     const std::string& latin1, std::string* result
 ) {
@@ -265,6 +281,44 @@ inline std::optional<CharHandlingError> Latin1ToBytes(
   }
 
   return std::nullopt;
+}
+
+/*!
+ \brief Convert a byte sequence to a Latin-1 string.
+ \param Bytes The input byte sequence.
+ \param result The output Latin-1 string.
+ The function will convert each byte in the input to a Latin-1 character.
+ For bytes in the range [0x00, 0x7F], the corresponding Latin-1 character is the same as the byte.
+ For bytes in the range [0x80, 0xFF], the corresponding Latin-1 character is represented by two
+ bytes in UTF-8:
+   - First byte:  110xxxxx (0xC0 | (byte >> 6))
+   - Second byte: 10xxxxxx (0x80 | (byte & 0x3F))
+  Example:
+    0xFF -> 0xC3 0xBF
+    'A' -> 'A'
+*/
+inline void ByteToLatin1(const std::string& bytes, std::string* result) {
+  result->clear();
+  const char* data = bytes.c_str();
+
+  for (int current_idx = 0; *(data + current_idx) != '\0'; current_idx++) {
+    const unsigned char& current_char = static_cast<unsigned char>(*(data + current_idx));
+
+    // Ascii character, directly add to result.
+    if (current_char <= 0x7F) {
+      result->push_back(static_cast<char>(current_char));
+      continue;
+    }
+
+    // not Ascii character, convert to Latin-1.
+    unsigned char latin1_first_byte = 0;
+    unsigned char latin1_second_byte = 0;
+
+    latin1_first_byte = 0xC0 | (current_char >> 6);
+    latin1_second_byte = 0x80 | (current_char & 0x3F);
+    result->push_back(static_cast<char>(latin1_first_byte));
+    result->push_back(static_cast<char>(latin1_second_byte));
+  }
 }
 
 inline int HexCharToInt(char c) {

cpp/support/json_serializer.h
@@ -348,7 +348,9 @@ inline picojson::value AutoSerializeJSONValue(const T& value) {
   } else if constexpr (std::is_floating_point_v<T>) {
     return picojson::value(static_cast<double>(value));
   } else if constexpr (std::is_same_v<T, std::string>) {
-    return picojson::value(value);
+    std::string result;
+    ByteToLatin1(value, &result);
+    return picojson::value(result);
   } else if constexpr (is_std_optional<T>::value) {
     if (value.has_value()) {
       return AutoSerializeJSONValue(*value);

tests/cpp/test_serialization.cc
@@ -253,7 +253,7 @@ TEST(XGrammarSerializationTest, TestString) {
   {
     std::string value = "\xC3\x28";
     auto json_value = AutoSerializeJSON(value);
-    ASSERT_EQ(json_value, "\"\\u00c3(\"");
+    ASSERT_EQ(json_value, u8"\"\u00c3(\"");
 
     std::string deserialized;
     auto error = AutoDeserializeJSON(&deserialized, json_value);
@@ -264,7 +264,7 @@ TEST(XGrammarSerializationTest, TestString) {
     std::string value = "我";
     auto json_value = AutoSerializeJSON(value);
     std::cout << json_value << std::endl;
-    ASSERT_EQ(json_value, "\"\\u00e6\\u0088\\u0091\"");
+    ASSERT_EQ(json_value, u8"\"\u00e6\u0088\u0091\"");
 
     std::string deserialized;
     auto error = AutoDeserializeJSON(&deserialized, json_value);

tests/python/test_json_schema_converter.py
@@ -2244,5 +2244,54 @@ root_repeat_2_3 ::= ("" | ([ \n\t])) (=("}"))
     assert not _is_grammar_accept_string(grammar, '{    "key"  :  "value"    }')
 
 
+def test_utf8_in_enum():
+    schema = {"type": "string", "enum": ["こんにちは", "😊", "你好", "hello", "\n"]}
+    grammar = xgr.Grammar.from_json_schema(schema)
+    assert _is_grammar_accept_string(grammar, '"こんにちは"')
+    assert _is_grammar_accept_string(grammar, '"😊"')
+    assert _is_grammar_accept_string(grammar, '"你好"')
+    assert _is_grammar_accept_string(grammar, '"hello"')
+    assert _is_grammar_accept_string(grammar, '"\\n"')
+
+
+def test_utf8_string_in_const():
+    schema = {"const": "常数constじょうすう\n\r\t"}
+    grammar = xgr.Grammar.from_json_schema(schema)
+    assert _is_grammar_accept_string(grammar, '"常数constじょうすう\\n\\r\\t"')
+
+
+def test_utf8_object_array_in_enum():
+    schema = {
+        "type": "object",
+        "enum": [
+            {"key": "こんにちは"},
+            {"key": "😊"},
+            {"key": "你好"},
+            {"key": "hello"},
+            {"key": "\n"},
+            [123, "こんにちは", "😊", "你好", "hello", "\n"],
+        ],
+    }
+    grammar = xgr.Grammar.from_json_schema(schema)
+    assert _is_grammar_accept_string(grammar, '{"key":"こんにちは"}')
+    assert _is_grammar_accept_string(grammar, '{"key":"😊"}')
+    assert _is_grammar_accept_string(grammar, '{"key":"你好"}')
+    assert _is_grammar_accept_string(grammar, '{"key":"hello"}')
+    assert _is_grammar_accept_string(grammar, '{"key":"\\n"}')
+    assert _is_grammar_accept_string(grammar, '[123,"こんにちは","😊","你好","hello","\\n"]')
+
+
+def test_utf8_object_const():
+    schema = {"type": "object", "const": {"key": "こんにちは常数constじょうすう\n\r\t"}}
+    grammar = xgr.Grammar.from_json_schema(schema)
+    assert _is_grammar_accept_string(grammar, '{"key":"こんにちは常数constじょうすう\\n\\r\\t"}')
+
+
+def test_utf8_array_const():
+    schema = {"type": "array", "const": ["こんにちは", "😊", "你好", "hello", "\n"]}
+    grammar = xgr.Grammar.from_json_schema(schema)
+    assert _is_grammar_accept_string(grammar, '["こんにちは","😊","你好","hello","\\n"]')
+
+
 if __name__ == "__main__":
     pytest.main(sys.argv)

tests/python/test_serialization.py
@@ -8,6 +8,7 @@ from pydantic import BaseModel, RootModel
 from transformers import AutoTokenizer  # type: ignore
 
 import xgrammar as xgr
+from xgrammar.testing import _is_grammar_accept_string
 
 
 def construct_grammar():
@@ -319,6 +320,18 @@ def test_serialize_compiled_grammar_with_hf_tokenizer():
 
     assert matcher.accept_token(tokenizer.eos_token_id)
     assert matcher.is_terminated()
+
+
+def test_serialize_grammar_utf8():
+    """Test Grammar serialization with UTF-8 characters."""
+    grammar = xgr.Grammar.from_ebnf('root ::= "こんにちは" | "😊" | "你好" | "hello" | "\\n"')
+    serialized = grammar.serialize_json()
+    recovered_grammar = xgr.Grammar.deserialize_json(serialized)
+    assert _is_grammar_accept_string(recovered_grammar, "こんにちは")
+    assert _is_grammar_accept_string(recovered_grammar, "😊")
+    assert _is_grammar_accept_string(recovered_grammar, "你好")
+    assert _is_grammar_accept_string(recovered_grammar, "hello")
+    assert _is_grammar_accept_string(recovered_grammar, "\n")
 
 
 if __name__ == "__main__":