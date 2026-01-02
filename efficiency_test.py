7ac743cedac386519f288a994fb732a5dd40cdb2
Yuchuan <yuchuan.7streams@gmail.com>
2026-01-02 10:51:12 +0800

    format.
    Signed-off-by: Yuchuan <yuchuan.7streams@gmail.com>

--- efficiency_test.py
+++ efficiency_test.py
@@ -1,78 +1,41 @@
-import xgrammar as xgr
-from transformers import AutoTokenizer
 import time
 
+from transformers import AutoTokenizer
+
+import xgrammar as xgr
+
 string_test_schemas = [
-    {
-        "type":"string",
-        "minLength": 10  
-    },
-    {
-        "type":"string",
-        "maxLength": 2048
-    },
-    {
-        "type":"string",
-        "minLength": 10,
-        "maxLength": 2048
-    },
-    {
-        "type":"string"
-    }
+    {"type": "string", "minLength": 10},
+    {"type": "string", "maxLength": 2048},
+    {"type": "string", "minLength": 10, "maxLength": 2048},
+    {"type": "string"},
 ]
 
 array_test_schemas = [
-    {
-        "type":"array",
-        "items": {"type": ["string", "number"]},
-        "minItems": 10 
-    },
-    {
-        "type":"array",
-        "items": {"type": ["string", "number"]},
-        "maxItems": 2048
-    },
-    {
-        "type":"array",
-        "items": {"type": ["string", "number"]},
-        "minItems": 10,
-        "maxItems": 2048
-    },
-    {
-        "type":"array",
-        "items": {"type": ["string", "number"]},
-    }
+    {"type": "array", "items": {"type": ["string", "number"]}, "minItems": 10},
+    {"type": "array", "items": {"type": ["string", "number"]}, "maxItems": 2048},
+    {"type": "array", "items": {"type": ["string", "number"]}, "minItems": 10, "maxItems": 2048},
+    {"type": "array", "items": {"type": ["string", "number"]}},
 ]
 
 object_test_schemas = [
     {
-        "type":"object",
-        "patternProperties": {
-            "^[a-zA-Z_][a-zA-Z_0-9]*$": { "type": "string" }
-        },
-        "minProperties": 10
-    },
-    {
-        "type":"object",
-        "patternProperties": {
-            "^[a-zA-Z_][a-zA-Z_0-9]*$": { "type": "string" }
-        },
-        "maxProperties": 2048
-    },
-    {
-        "type":"object",
-        "patternProperties": {
-            "^[a-zA-Z_][a-zA-Z_0-9]*$": { "type": "string" }
-        },
+        "type": "object",
+        "patternProperties": {"^[a-zA-Z_][a-zA-Z_0-9]*$": {"type": "string"}},
         "minProperties": 10,
-        "maxProperties": 2048
     },
     {
-        "type":"object",
-        "patternProperties": {
-            "^[a-zA-Z_][a-zA-Z_0-9]*$": { "type": "string" }
-        },
+        "type": "object",
+        "patternProperties": {"^[a-zA-Z_][a-zA-Z_0-9]*$": {"type": "string"}},
+        "maxProperties": 2048,
     },
+    {
+        "type": "object",
+        "patternProperties": {"^[a-zA-Z_][a-zA-Z_0-9]*$": {"type": "string"}},
+        "minProperties": 10,
+        "maxProperties": 2048,
+    },
+    {"type": "object", "patternProperties": {"^[a-zA-Z_][a-zA-Z_0-9]*$": {"type": "string"}}},
 ]
 
 
@@ -84,26 +47,27 @@ tokenizer_info = xgr.TokenizerInfo.from_huggingface(tokenizer=tokenizer)
 token_bitmask = xgr.allocate_token_bitmask(1, tokenizer_info.vocab_size)
 compiler = xgr.GrammarCompiler(tokenizer_info, cache_enabled=False)
 
-warmup_schema = {
-    "type":"object"
-}
+warmup_schema = {"type": "object"}
 
 _ = compiler.compile_json_schema(warmup_schema)
 
+
 def test_schema_length(schemas: list[dict], lengths: list[int]):
     for length in lengths:
-        test_string = "\""
+        test_string = '"'
         for i in range(length):
             test_string += "a"
-        test_string += "\""
-    
+        test_string += '"'
+
         for schema in schemas:
             start_time = time.monotonic_ns()
             compiled_grammar = compiler.compile_json_schema(schema)
             matcher = xgr.GrammarMatcher(compiled_grammar)
             compile_time = time.monotonic_ns() - start_time
-            print(f"Schema: {schema}, String Length: {length}, Compile Time: {compile_time / 1000:.2f} us")
-            
+            print(
+                f"Schema: {schema}, String Length: {length}, Compile Time: {compile_time / 1000:.2f} us"
+            )
+
             tpots = []
             for character in test_string:
                 start_time = time.monotonic_ns()
@@ -112,7 +76,10 @@ def test_schema_length(schemas: list[dict], lengths: list[int]):
                 tpot_time = time.monotonic_ns() - start_time
                 tpots.append(tpot_time)
             avg_tpot = sum(tpots) / len(tpots)
-            print(f"Schema: {schema}, String Length: {length}, Average TPoT: {avg_tpot / 1000:.2f} us")
+            print(
+                f"Schema: {schema}, String Length: {length}, Average TPoT: {avg_tpot / 1000:.2f} us"
+            )
+
 
 def test_array_length(schemas: list[dict], lengths: list[int]):
     for length in lengths:
@@ -121,16 +88,18 @@ def test_array_length(schemas: list[dict], lengths: list[int]):
             if i % 2 == 0:
                 test_array += "0,"
             else:
-                test_array += "\"a\","
+                test_array += '"a",'
         test_array += "]"
-    
+
         for schema in schemas:
             start_time = time.monotonic_ns()
             compiled_grammar = compiler.compile_json_schema(schema)
             matcher = xgr.GrammarMatcher(compiled_grammar)
             compile_time = time.monotonic_ns() - start_time
-            print(f"Schema: {schema}, Array Length: {length}, Compile Time: {compile_time / 1000:.2f} us")
-            
+            print(
+                f"Schema: {schema}, Array Length: {length}, Compile Time: {compile_time / 1000:.2f} us"
+            )
+
             tpots = []
             for character in test_array:
                 start_time = time.monotonic_ns()
@@ -139,23 +108,28 @@ def test_array_length(schemas: list[dict], lengths: list[int]):
                 tpot_time = time.monotonic_ns() - start_time
                 tpots.append(tpot_time)
             avg_tpot = sum(tpots) / len(tpots)
-            print(f"Schema: {schema}, Array Length: {length}, Average TPoT: {avg_tpot / 1000:.2f} us")
+            print(
+                f"Schema: {schema}, Array Length: {length}, Average TPoT: {avg_tpot / 1000:.2f} us"
+            )
+
 
 def test_object_length(schemas: list[dict], lengths: list[int]):
     for length in lengths:
         test_object = "{"
         for i in range(length):
-            test_object += f"\"key{i}\": \"value{i}\","
+            test_object += f'"key{i}": "value{i}",'
         test_object += "}"
-    
+
         for schema in schemas:
             start_time = time.monotonic_ns()
             compiled_grammar = compiler.compile_json_schema(schema)
             matcher = xgr.GrammarMatcher(compiled_grammar)
             compile_time = time.monotonic_ns() - start_time
-            print(f"Schema: {schema}, Object Length: {length}, Compile Time: {compile_time / 1000:.2f} us")
+            print(
+                f"Schema: {schema}, Object Length: {length}, Compile Time: {compile_time / 1000:.2f} us"
+            )
             print(compiled_grammar.grammar)
-            
+
             tpots = []
             for character in test_object:
                 start_time = time.monotonic_ns()
@@ -165,7 +139,10 @@ def test_object_length(schemas: list[dict], lengths: list[int]):
                 tpots.append(tpot_time)
                 print(f"Char: {character}, TPoT: {tpot_time / 1000:.2f} us")
             avg_tpot = sum(tpots) / len(tpots)
-            print(f"Schema: {schema}, Object Length: {length}, Average TPoT: {avg_tpot / 1000:.2f} us")
+            print(
+                f"Schema: {schema}, Object Length: {length}, Average TPoT: {avg_tpot / 1000:.2f} us"
+            )
+
 
 # test_schema_length(string_test_schemas, length)
 # test_array_length(array_test_schemas, length)
