3f2d844fc42a4a0caff03a29b7cdd9315490f0cf
Yuchuan <yuchuan.7streams@gmail.com>
2026-01-04 21:12:44 +0800

    format.
    Signed-off-by: Yuchuan <yuchuan.7streams@gmail.com>

--- tests/python/test_function_calling_converter.py
+++ tests/python/test_function_calling_converter.py
@@ -89,7 +89,6 @@ root ::=  [ \n\t]* (("<parameter=name>" [ \n\t]* xml_string_0 [ \n\t]* "</parame
         "additionalProperties": True,
     }
     ebnf_grammar = _qwen_xml_tool_calling_to_ebnf(schema)
-    print(ebnf_grammar)
     assert str(ebnf_grammar[:-2]) == expected_grammar
     assert _is_grammar_accept_string(ebnf_grammar, input_str) == accepted
 
