df708eb95dc179f96f985d4b962611d6bfe951e6
Lysandros Nikolaou <lisandrosnik@gmail.com>
2026-01-07 15:47:11 +0200

    Add 3.14, 3.14t and pytest-run-parallel to CI

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+
+try:
+    import pytest_run_parallel  # noqa: F401
+
+    PARALLEL_RUN_AVALIABLE = True
+except ModuleNotFoundError:
+    PARALLEL_RUN_AVALIABLE = False
+
+
+def pytest_configure(config):
+    if not PARALLEL_RUN_AVALIABLE:
+        config.addinivalue_line(
+            "markers", "thread_unsafe: mark the test function as single-threaded"
+        )
+
+
+if not PARALLEL_RUN_AVALIABLE:
+
+    @pytest.fixture
+    def num_parallel_threads():
+        return 1
