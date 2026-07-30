9b1131d3453cc3f9f0e82817f1f39b9e753efa9b
Ubospica <ubospica@gmail.com>
2026-07-30 02:15:57 -0400

    feat: support word-aligned substring regexes

---
+++ cpp/support/unicode_char_class.h
@@ -0,0 +1,21 @@
+/*!
+ *  Copyright (c) 2026 by Contributors
+ * \file xgrammar/support/unicode_char_class.h
+ * \brief Locale-independent Unicode character classification.
+ */
+#ifndef XGRAMMAR_SUPPORT_UNICODE_CHAR_CLASS_H_
+#define XGRAMMAR_SUPPORT_UNICODE_CHAR_CLASS_H_
+
+#include "encoding.h"
+
+namespace xgrammar {
+
+/*! \brief Return whether a codepoint has the Unicode White_Space property. */
+bool IsUnicodeWhitespace(TCodepoint codepoint);
+
+/*! \brief Return whether a codepoint is Unicode alphabetic or numeric. */
+bool IsUnicodeAlphanumeric(TCodepoint codepoint);
+
+}  // namespace xgrammar
+
+#endif  // XGRAMMAR_SUPPORT_UNICODE_CHAR_CLASS_H_
